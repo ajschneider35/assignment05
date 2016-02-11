962f7d499a43bb92edde8475e90bc6e7269b09ec
Andrew Schneider <andrewj.schneider@utah.edu>
2016-02-11 09:35:14 -0700

    Initial commit of files

---
+++ assignment05/src/assignment05/SortUtil.java
@@ -0,0 +1,66 @@
+package assignment05;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * FILL IN
+ * @author Andrew Schneider
+ * @author Connor Ottenbacher
+ * @version 2/18/2016
+ *
+ */
+public class SortUtil {
+	
+	/**
+	 * This method performs a mergesort on the generic ArrayList given as input.
+	 * Driver method.
+	 * @param list
+	 * @param cmp
+	 */
+	public static <T> void mergesort(ArrayList<T> list, Comparator<? super T> cmp) {
+		//TODO
+	}
+	
+	/**
+	 * This method performs a quicksort on the generic ArrayList given as input.
+	 * Driver method.
+	 * @param list
+	 * @param cmp
+	 */
+	public static <T> void quicksort(ArrayList<T> list, Comparator<? super T> cmp) {
+		//TODO
+	}
+	
+	/**
+	 * This method generates and returns an ArrayList of integers 1 to size in ascending order.
+	 * @param size
+	 * @return
+	 */
+	public static ArrayList<Integer> generateBestCase(int size) {
+		//TODO
+		return new ArrayList<Integer>();
+	}
+	
+	/**
+	 * This method generates and returns an ArrayList of integers 1 to size in permuted order 
+	 * (i,e., randomly ordered). 
+	 * @param size
+	 * @return
+	 */
+	public static ArrayList<Integer> generateAverageCase(int size) {
+		//TODO
+		return new ArrayList<Integer>();
+	}
+	
+	/**
+	 * This method generates and returns an ArrayList of integers 1 to size in descending order.
+	 * @param size
+	 * @return
+	 */
+	public static ArrayList<Integer> generateWorstCase(int size) {
+		//TODO
+		return new ArrayList<Integer>();
+	}
+
+}
