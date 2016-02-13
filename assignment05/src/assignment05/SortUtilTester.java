d40ee27fbec265d08b5147c7ca0edcad5766f0c7
Andrew Schneider <andrewj.schneider@utah.edu>
2016-02-12 19:16:10 -0700

    Added multiple tests for quicksort

--- assignment05/src/assignment05/SortUtilTester.java
+++ assignment05/src/assignment05/SortUtilTester.java
@@ -25,12 +25,54 @@ public class SortUtilTester {
 		ArrayList<Integer> list = SortUtil.generateAverageCase(200);
 		Collections.sort(result);
 
-		class sortInteger implements Comparator<Integer> {
-
-			public int compare(Integer int1, Integer int2) {
-				return int1.compareTo(int2);
-			}
+		SortUtil.quicksort(list, new sortInteger());
+		
+		for(int i = 0; i < list.size(); i++)
+			assertEquals(result.get(i), list.get(i));
+	}
+	
+	@Test
+	public void QuicksortEmptyList() {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		SortUtil.quicksort(list, new sortInteger());
+		
+		assertEquals(result.size(), list.size());
+		assertEquals(0, list.size());
+	}
+	
+	@Test
+	public void QuicksortAlreadySorted() {
+		ArrayList<Integer> result = SortUtil.generateBestCase(200);
+		ArrayList<Integer> list = SortUtil.generateBestCase(200);
+		
+		SortUtil.quicksort(list, new sortInteger());
+		
+		for(int i = 0; i < list.size(); i++)
+			assertEquals(result.get(i), list.get(i));
+	}
+	
+	@Test
+	public void QuicksortReverseSorted() {	
+		ArrayList<Integer> result = SortUtil.generateBestCase(200);
+		ArrayList<Integer> list = SortUtil.generateWorstCase(200);
+		
+		SortUtil.quicksort(list, new sortInteger());
+		
+		for(int i = 0; i < list.size(); i++)
+			assertEquals(result.get(i), list.get(i));
+	}
+	
+	@Test
+	public void QuicksortAllTheSameItemsInList() {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < 200; i++) {
+			result.add(1);
+			list.add(1);
 		}
+		
 		SortUtil.quicksort(list, new sortInteger());
 		
 		for(int i = 0; i < list.size(); i++)
@@ -43,12 +85,6 @@ public class SortUtilTester {
 		ArrayList<Integer> list = SortUtil.generateAverageCase(200);
 		Collections.sort(result);
 
-		class sortInteger implements Comparator<Integer> {
-
-			public int compare(Integer int1, Integer int2) {
-				return int1.compareTo(int2);
-			}
-		}
 		SortUtil.mergesort(list, new sortInteger());
 		
 		for(int i = 0; i < list.size(); i++)
@@ -62,17 +98,19 @@ public class SortUtilTester {
 		test.add(1);
 		test.add(2000);
 		test.add(2000);
-		class sortInteger implements Comparator<Integer> {
 
-			public int compare(Integer int1, Integer int2) {
-				return int1.compareTo(int2);
-			}
-		}
 		Random rand = new Random();
 
 		assertEquals((Integer) 2000, SortUtil.findPivot(test, 0, test.size() - 1,
 				"Median", new sortInteger(), rand));
 
+	}
+	
+	public class sortInteger implements Comparator<Integer> {
+
+		public int compare(Integer int1, Integer int2) {
+			return int1.compareTo(int2);
+		}
 	}
 
 }
